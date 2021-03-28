1448d645447b4ab264249e7add1c6cad941484f7
Agustí Moll <agusti@gmail.com>
2021-03-28 23:46:54 +0200

    Add program to encript in Xab protocol

---
+++ act2/xab.js
@@ -0,0 +1,115 @@
+/*
+
+Cripto sistema - Crear claus:
+ 
+1. Crear 2 enters aleatoris a, b
+
+2. calcular M  = ab - 1
+
+3. Crear 2 enters aleatoris a', b'
+
+4. Calcular e, d i n
+    e = a'·M + a
+    d = b'·M + b
+    n = (e*d - 1) / M
+
+5. Calular claus pk = ( n, e) i sk = (d[, n])
+
+Encript
+
+Epk(m) = m*e mod n
+
+
+Descript 
+Dsk(c) = c*d mod n
+
+
+*/
+const BigNumber = require('bignumber.js');
+
+const MAX_DEC = 50
+
+BigNumber.set({ DECIMAL_PLACES: MAX_DEC })
+const MAX_RND = BigNumber(10).pow(MAX_DEC)
+const ℕ = true
+
+
+aleatorEnter = function (max) { // Retorna un BigNumber
+    let rnd = BigNumber.random().multipliedBy(max).integerValue()
+    if (ℕ) {
+        return rnd
+    }
+    // Aquí tenim un numero N el convertim en Z fent que dividim per 2 els imparella son els negatius i parells positius.
+    if ( rnd.mod(2) == 1 ) {
+        return rnd.idiv(2).times(-1)
+    } else {
+        return rnd.idiv(2)
+    } 
+}
+
+
+calculateKeys = function () {
+    cs = {}
+
+    cs.a =  aleatorEnter(MAX_RND)
+    cs.b =  aleatorEnter(MAX_RND)
+    cs.a2 = aleatorEnter(MAX_RND)
+    cs.b2 = aleatorEnter(MAX_RND)
+/*
+    cs.a = BigNumber(16)
+    cs.b = BigNumber(20)
+    cs.a2 = BigNumber(18)
+    cs.b2 = BigNumber(14).times(-1)
+*/
+    cs.M = cs.a.multipliedBy(cs.b).minus(1)
+    cs.e = cs.a2.multipliedBy(cs.M).plus(cs.a)
+    cs.d = cs.b2.multipliedBy(cs.M).plus(cs.b)
+    cs.n = (cs.e.multipliedBy(cs.d).minus(1)).div(cs.M)
+
+    cs.pk = {k : cs.e, n : cs.n}
+    cs.sk = {k : cs.d, n: cs.n}
+
+
+    return cs
+}
+
+
+enc = function (pk, m) {
+    return  m.multipliedBy(pk.k).mod(pk.n)
+}
+
+dec = function (sk, e) {
+    return e.multipliedBy(sk.k).mod(sk.n)
+}
+
+toPrintCS = function(cs) {
+    console.log("cs.a =", cs.a.toFixed())
+    console.log("cs.b =", cs.b.toFixed())
+    console.log("cs.a2 =", cs.a2.toFixed())
+    console.log("cs.b2 =", cs.b2.toFixed())
+    console.log("cs.M =", cs.M.toFixed())
+    console.log("cs.e =", cs.e.toFixed())
+    console.log("cs.d =", cs.d.toFixed())
+    console.log("cs.n =", cs.n.toFixed())
+
+}
+
+
+cryptoSystem = calculateKeys()
+toPrintCS(cryptoSystem)
+
+missatge = aleatorEnter(MAX_RND)
+//missatge = BigNumber(15)
+console.log("missatge =",missatge.toFixed())
+
+mencrypt = enc(cryptoSystem.pk, missatge)
+console.log("encriptat =",mencrypt.toFixed())
+
+mdecrypt = dec(cryptoSystem.sk, mencrypt)
+console.log("desencriptat =",mdecrypt.toFixed())
+
+if (missatge.eq(mdecrypt)) {
+    console.log("Correcte!")
+} else {
+    console.log("Incorrecte!")
+}
